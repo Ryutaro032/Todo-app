b7d34485340457e452b4c48fa984948bb40f6b68
Ryutaro032 <motti.k32@gmail.com>
2023-04-10 20:32:09 +0900

    css bootstrap 実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,12 +7,13 @@ use App\Models\Folder;
 
 class TaskController extends Controller
 {
-    public function index(){
+    public function index(int $id){
         $model = new Folder();
         $folders = $model->getFolder();
 
         return view('tasks/index', [
             'folders' => $folders,
+            'current_folder_id' => $id,
         ]);
     }
 }
--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To do App</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}"></script>
 </head>
 <body>
     <header>
@@ -16,7 +18,7 @@
     <main>
         <div class="container">
             <div class="row">
-                <div class="col">
+                <div class="col col-md-4">
                     <nav class="panel">
                         <div class="panel-heading">フォルダ</div>
                         <div class="panel-body">
@@ -26,13 +28,15 @@
                         </div>
                         <div class="list-group">
                             @foreach($folders as $folder)
-                            <a href="{{ route('tasks.index', ['id' => $folder->id]) }}" class="list-group-item">
+                            <a href="{{ route('tasks.index', ['id' => $folder->id]) }}" 
+                               class="list-group-item {{ $current_folder_id === $folder->id ? 'active' : '' }}"
+                            >
                                 {{ $folder->title }}
                             </a>
                             @endforeach
                         </div>
                     </nav>
-                    <div class="column">
+                    <div class="column col-md-8">
                         <!-- ここにタスクが表示される -->
                     </div>
                 </div>
